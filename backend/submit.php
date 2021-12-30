25a86c1a08c98d9bbbacbe99dccca8d8140b9067
Jay Dhanani <jaydhanani36.jd@gmail.com>
2021-12-30 19:41:06 +0530

    Added form submission in index.html

--- backend/submit.php
+++ backend/submit.php
@@ -5,7 +5,7 @@ include_once('database.php');
 
 $objStmt = $objConn->prepare("INSERT INTO  subscribers (email) VALUES (?)");
 
-$objStmt->bind_param($_REQUEST['Email-2']);
+$objStmt->bind_param("s", $_REQUEST['Email-2']);
 $objStmt->execute();
 
 $objStmt->close();
