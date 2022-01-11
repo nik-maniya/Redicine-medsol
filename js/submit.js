68b5d80760f788e79932c708635087bb2dc143b0
Nikunj <writetonikunj98@gmail.com>
2022-01-11 17:08:38 +0530

    NewChanges

--- js/submit.js
+++ js/submit.js
@@ -1,6 +1,10 @@
 
 $("#subscribe_submit").click(function(){
-    if($('#Email-2').val()){
+    if($('#Email-2').val() && String($('#Email-2').val())
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
         $.ajax({
             type: "POST",
             url: window.location.protocol+'//'+window.location.host+'/backend/submit.php',
